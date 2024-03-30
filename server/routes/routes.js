ce37dd89467b5e8231fc433115351a2d8a08ce10
Keshav Gupta <keshav.guptaa99@gmail.com>
2024-03-30 23:52:09 +0530

    server(routes): Use users router
    Signed-off-by: Keshav Gupta <keshav.guptaa99@gmail.com>

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import loginRouter from './login/login.js';
 import logoutRouter from './logout/logout.js';
+import usersRouter from './users/users.js';
 const router = Router({ mergeParams: true });
 
 router.use('/login', loginRouter);
 router.use('/logout', logoutRouter);
+router.use('/users', usersRouter);
 
 export default router;
