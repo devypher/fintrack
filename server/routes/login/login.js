e2ba7f0e8acab54f3ca2d5fad3e67e38d0d7d546
Keshav Gupta <keshav.guptaa99@gmail.com>
2024-03-30 23:49:36 +0530

    routes(login): Add post routes
    Endpoint to be used for logging in with email and password
    
    Signed-off-by: Keshav Gupta <keshav.guptaa99@gmail.com>

--- server/routes/login/login.js
+++ server/routes/login/login.js
@@ -1,9 +1,27 @@
 import { Router } from 'express';
 import oauthRouter from './oauth/oauth.js';
 import { returnIfAuthenticated } from '../middleware/common.js';
+import { getUserCookie } from '../../controllers/login/login.js';
+import { AUTH_COOKIE } from '../../configs/login.js';
 const loginRouter = Router({ mergeParams: true });
 
 loginRouter.use(returnIfAuthenticated);
+
+loginRouter.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  const userCookie = await getUserCookie(email, password);
+
+  if (userCookie.error) {
+    res.status(403).json({
+      error: userCookie.error,
+    });
+    return;
+  }
+
+  res.cookie(AUTH_COOKIE, userCookie.cookie, { httpOnly: true });
+  res.status(200).end();
+});
 
 loginRouter.use('/auth', oauthRouter);
 
