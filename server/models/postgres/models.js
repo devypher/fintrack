f0096d66f8b2202a373ae61b73d51f3ad7913062
Keshav Gupta <keshav.guptaa99@gmail.com>
2024-04-01 01:50:29 +0530

    postgres(models): Add Friend and FriendRequest
    Signed-off-by: Keshav Gupta <keshav.guptaa99@gmail.com>

--- server/models/postgres/models.js
+++ server/models/postgres/models.js
@@ -1,7 +1,11 @@
 import User from './User.js';
+import Friend from './Friend.js';
+import FriendRequest from './FriendRequest.js';
 
 const DB_MODELS = {
   User,
+  Friend,
+  FriendRequest,
 };
 
 export default DB_MODELS;
